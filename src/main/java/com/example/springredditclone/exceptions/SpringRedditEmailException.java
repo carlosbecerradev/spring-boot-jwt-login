ebc49bd34b7dd345cbc43f79942c0ec41b1dcc68
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-07-31 18:46:56 -0500

    user registration

---
+++ src/main/java/com/example/springredditclone/exceptions/SpringRedditEmailException.java
@@ -0,0 +1,8 @@
+package com.example.springredditclone.exceptions;
+
+public class SpringRedditEmailException extends RuntimeException {
+
+	public SpringRedditEmailException(String exMessage) {
+		super(exMessage);
+	}
+}
